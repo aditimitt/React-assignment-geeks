4e0ddba12b2d40173be200f7723d220ebc498897
aditimitt <aditimittalreach@gmail.com>
2024-09-20 21:19:01 +0530

    react assignment

--- src/types.d.ts
+++ src/types.d.ts
@@ -1,9 +1,11 @@
 // src/types.d.ts
 export interface Candidate {
-    id: number;
+    _id: number;
     name: string;
     skills: string;
     experience: number;
     location: string;
+    videoInterviewResults?: string; // Optional, if not always present
+    codingResults?: string; // Optional, if not always present
   }
   
